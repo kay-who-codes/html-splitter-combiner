183e94f882102d5b9edcba0965242811c687abb4
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2025-11-09 13:15:14 +0000

    Create CopyFeedback.js

---
+++ src/components/CopyFeedback.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CopyFeedback({ show }) {
+  return (
+    <div className={`copy-feedback ${show ? 'show' : ''}`}>
+      ✓ Copied to clipboard!
+    </div>
+  );
+}
+
+export default CopyFeedback;
